af056768e71dd7e98ed0c26ecc2ebdf6e29d3145
Nikita Genchev <s1228224@naval-acad.bg>
2021-08-23 22:50:26 +0300

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-const app = require("../app");
+var app = require("../app");
 var path = require('path');
-
-
 var url = 'mongodb://localhost:27017';
 
 router.use(function (req, res, next) {
     MongoClient.connect(url, async function (err, db) {
         if (err) throw err;
-        var dbo = await db.db("online_shop");
+        const dbo = await db.db("online_shop");
         await dbo.collection("categories").find({}, {projection: {name: 1, id: 1}}).toArray(function (err, result) {
             if (err) throw err;
             res.locals.navbar = result;
@@ -20,15 +17,14 @@ router.use(function (req, res, next) {
     });
 })
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index.ejs', {products: false, product: false});
 });
 
 router.get('/get-products/:category', function (req, res) {
     MongoClient.connect(url, function (err, db) {
-        var dbo = db.db("online_shop");
-        var category = req.params.category;
+        const dbo = db.db("online_shop");
+        const category = req.params.category;
         dbo.collection("products").find({
             primary_category_id: category,
         }, {
@@ -50,31 +46,14 @@ router.get('/get-products/:category', function (req, res) {
 
 router.get('/get-product/:id', function (req, res) {
     MongoClient.connect(url, function (err, db) {
-        var dbo = db.db("online_shop");
-        var productId = req.params.id;
+        const dbo = db.db("online_shop");
+        const productId = req.params.id;
         dbo.collection("products").find({
                 id: productId,
             }
         ).toArray(function (err, result) {
             if (err) throw err;
             res.render('index.ejs', {product: result, products: false})
-            console.log(result);
-            //res.send(result);
-        });
-    });
-})
-router.get('/filter/', function (req, res, next) {
-    let filter = req.query;
-    let neshto = {};
-
-    MongoClient.connect(url, function (err, db) {
-        var dbo = db.db("online_shop");
-        var productId = req.params.id;
-        dbo.collection("products").find(filter
-        ).toArray(function (err, result) {
-            if (err) throw err;
-            res.render('index.ejs', {product: result, products: false})
-            console.log(result);
             //res.send(result);
         });
     });
@@ -82,6 +61,5 @@ router.get('/filter/', function (req, res, next) {
 
 router.use('/get-products/:category', express.static(path.join(__dirname, '/public')));
 router.use('/get-product/:product', express.static(path.join(__dirname, '/public')));
-
 
 module.exports = router;
